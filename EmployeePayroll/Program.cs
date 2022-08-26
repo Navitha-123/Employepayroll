abce9f1405dd611e1deb5ce59f4dea03f92cf3c8
navitha <navithareddy112@gmail.com>
2022-08-26 11:08:30 +0530

    [navitha] EmpPresentAbsent

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -5,15 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Payroll");
-            Console.WriteLine("choose option\n 1.check absent ");
+            Console.WriteLine("choose option\n 1.check absent \t 2. DailyWage \t 3. PartTimeWage \t 4.emp wage for month ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
             case 1:
                  EmployeeUCpresentorabsent.EmpAttendenceCheck();
                     break;
-
-            default:
+                case 2:
+                    DailyWageUC2.CalculateDailyWage();
+                    break;
+                case3:
+                    PartTimeWage.CalculatePercentage();
+                case4:
+                    WageForMonth.CalculateWageForMonth();
+                default:
                     Console.WriteLine( "Enterd wrong choice");
                 break;
             }            
