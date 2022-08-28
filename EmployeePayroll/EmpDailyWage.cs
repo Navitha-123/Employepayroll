01e9e3d6e34856dfae3e3f2fdd323d43a9620052
navitha <navithareddy112@gmail.com>
2022-08-28 11:41:48 +0530

    [navitha] PartTimeWages

--- EmployeePayroll/EmpDailyWage.cs
+++ EmployeePayroll/EmpDailyWage.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EmployeePayroll
 {
-    internal class DailyWageUC2
+    internal class EmpDailyWage
     {
         public static void CalculateDailyWage()
         {
